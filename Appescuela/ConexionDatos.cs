282ba9ac0d765983d5cdbfb6940c4c61f005756e
angel <angel@DESKTOP-5MDNF5H>
2025-05-16 18:57:53 -0400

    Agregar archivos de proyecto.

---
+++ Appescuela/ConexionDatos.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace  capaDatos
+{
+
+
+    public class personas
+    {
+
+        //atributos de la clase personas para ser heredados en otras clases 
+        public int ID;
+        public string Nombre;
+        public string apellido;
+        public string Tipo; 
+
+    }
+    //clase estudiante para heredar de la clase personas sus atributos 
+
+    public class estudiante : personas
+
+    {
+        public string carrera = "Ingeneria de Software";
+
+    }
+
+    public class Docente : personas
+
+    { 
+     public string materia = "Programacion";
+
+    }
+
+    public class personaDatos
+
+    {
+
+        public string conexion = "server=.;Database=escuela;integrated security=true";
+
+
+
+        public personas obtenerPersonasID(int idBusqueda)
+
+        {
+            using (SqlConnection conn = new SqlConnection(conexion))
+
+            {
+
+                conn.Open();
+
+                string query = "Select * from personas WHERE Id = @ID";
+            }
+
+
+        }
+
+
+
+    }
+
+
+}
